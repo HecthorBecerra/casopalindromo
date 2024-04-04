cde80798d8c6388fe990b36476922bc846979930
HectorBecerra <h.becerra01@ufromail.cl>
2024-04-04 15:05:06 -0300

    implementacion de preguntar a el usuario para que este ingrese la cadena

--- src/main/java/Palindromo.java
+++ src/main/java/Palindromo.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Palindromo {
 
-    public static boolean esPalindromo(String cadena) {
+    public static boolean verificadorPalindromo(String cadena) {
         String resultado = "";
 
         for (int i = cadena.length() - 1; i >= 0; i--) {
@@ -11,4 +11,15 @@ public class Palindromo {
 
         return cadena.equals(resultado);
     }
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese la palabra: ");
+        String respuesta = scanner.nextLine();
+        boolean esPalindromo = verificadorPalindromo(respuesta);
+        System.out.println("La cadena " + respuesta + " ¿Es un palindromo?: " + esPalindromo);
+
+    }
+
 }
